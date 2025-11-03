d387980597dd2be4cd8dcb86bb34bcda9589fc6d
singhmahipal <mahipalsingh052025@gmail.com>
2025-11-03 18:17:12 +0530

    fetch data from dashboard

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const { HoldingsModel } = require("./model/HoldingsModel");
 const { PositionsModel } = require("./model/PositionsModel");
 
@@ -9,6 +12,9 @@ const PORT = process.env.PORT || 8080;
 const url = process.env.MONGO_URL;
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
 
 // app.get('/addHoldings', async (req, res) => {
 //     let tempHoldings = [
@@ -179,6 +185,16 @@ const app = express();
 
 //   res.send("done!");
 // });
+
+app.get('/allHoldings', async (req, res) => {
+  let allHoldings = await HoldingsModel.find({});
+  res.json(allHoldings);
+});
+
+app.get('/allPositions', async (req, res) => {
+  let allPositions = await PositionsModel.find({});
+  res.json(allPositions);
+});
 
 app.listen(PORT, () => {
   console.log("app started!");
