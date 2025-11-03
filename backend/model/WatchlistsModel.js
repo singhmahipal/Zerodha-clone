d15de9f491e0050b54fea971f756d23b9670f0b0
singhmahipal <mahipalsingh052025@gmail.com>
2025-11-03 09:29:01 +0530

    fix: order schema & model

--- backend/model/WatchlistsModel.js
+++
@@ -1,7 +0,0 @@
-const {model} = require('mongoose');
-
-const {WatchlistsSchema} = require('../schemas/WatchlistsSchema.js');
-
-const WatchlistsModel = new model('watchlist', WatchlistsSchema);
-
-module.exports = {WatchlistsModel};
